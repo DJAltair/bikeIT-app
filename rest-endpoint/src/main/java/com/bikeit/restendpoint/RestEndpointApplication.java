dc56393d42eaa27bd392c6885e20ae4e23d175e3
DJAltair <jakub.piotrowski1@gmail.com>
2024-05-14 16:06:50 +0200

    initial commit

---
+++ rest-endpoint/src/main/java/com/bikeit/restendpoint/RestEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.bikeit.restendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEndpointApplication.class, args);
+	}
+
+}
